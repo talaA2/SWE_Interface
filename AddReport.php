b285cefddfeecd479e43fbcebba8294d6393f299
Jood <445201348@student.ksu.edu.sa>
2026-04-30 23:07:58 +0300

    Update AddReport.php

--- AddReport.php
+++ AddReport.php
@@ -77,7 +77,7 @@ $conn->query($notifSQL);
         <a href="MyReports.php" class="nav-link"><i class="fa-regular fa-clipboard"></i> My Reports</a>
         <a href="Rewards.php" class="nav-link"><i class="fa-regular fa-star"></i> Rewards</a>
         <a href="Notifications.php" class="nav-link"><i class="fa-regular fa-bell"></i> Notifications</a>
-        <a href="index.php" class="nav-link logout"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
+        <a href="logout.php" class="nav-link logout"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
       </nav>
     </div>
   </header>
